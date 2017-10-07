3be39ca5f3ad84dacf4953341dd8ecb45ef74dc0
Nimish David Mathew <experionnimish@users.noreply.github.com>
2017-10-08 01:33:13 +0530

    Header and Footer added

---
+++ src/app/app.component.ts
@@ -0,0 +1,6 @@
+import {Component} from '@angular/core';
+
+@Component({selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.sass']})
+export class AppComponent {
+  toggleNav = false
+}
